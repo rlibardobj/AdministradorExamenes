ddf36959713412af8dd4c9c7f6a568e9da31e042
rlibardobj <rlibardobj@gmail.com>
2013-11-17 18:56:02 -0600

    Archivos base del proyecto.

---
+++ AdministradorExamenes/src/administradorexamenes/AdministradorExamenes.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package administradorexamenes;
+
+/**
+ *
+ * @author Ronny
+ */
+public class AdministradorExamenes {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
